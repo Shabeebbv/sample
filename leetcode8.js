9151d59a544c0e113552c2780f14b5978af8e577
Mohammed shabeeb <sabishabeeb2@gmail.com>
2025-08-16 21:31:28 +0530

    message

---
+++ leetcode8.js
@@ -0,0 +1,9 @@
+
+// var isPowerOfTwo = function(n) {
+//     let powr=Math.pow(2,n)
+//     console.log(powr);
+    
+//     if (powr==)
+    
+// };
+// isPowerOfTwo(16)
---
+++ leetcode9.js
@@ -0,0 +1,14 @@
+var reverseString = function(s) {
+    // console.log(s)
+    s.reverse()
+    console.log(s)
+    // let x=s.split()
+    // console.log(x)
+    // let y=s.join("")
+    // console.log(y);
+    
+
+    
+
+};
+reverseString(["h","e","l","l","o"])
